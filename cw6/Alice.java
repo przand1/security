a6449c3e21d77b970d8014aff5c4f72c1d3dfb24
przand1 <przand1@st.amu.edu.pl>
2019-01-23 00:44:50 +0100

    cw6.2

--- cw6/Alice.java
+++ cw6/Alice.java
@@ -1,23 +1,31 @@
 package cw6;
 
 import java.io.*;
+import java.util.List;
 
 public class Alice {
 
-  private static final int VERTS = 30;
-  private static final int EDGES = 20;
+  private static final int VERTS = 12000;
+  private static final int EDGES = VERTS*VERTS/4;
 
   public static void main(String[] args) {
-    HGraph graph = new HGraph(VERTS);
+    HGraph graph = new HGraph(VERTS,EDGES);
     graph.generate();
-    System.out.println(graph.getHcycle().toString());
-    boolean[][] l = graph.getGraphTable();
-    for (boolean[]  bT : l ) {
+    System.out.println("BASE_G_CYCLE: "+graph.getHCycle().toString());
+    printGraph(graph.getGraphTable());
+    List<Integer> iso = graph.isomorphism();
+    System.out.println("ISO: "+iso.toString());
+    HGraph isoGraph = new HGraph(graph,iso);
+    System.out.println("ISO_G_CYCLE: "+isoGraph.getHCycle().toString());
+    printGraph(isoGraph.getGraphTable());
+
+  }
+
+  public static void printGraph(boolean[][] graph) {
+    for (boolean[]  bT : graph ) {
       for (boolean b:bT) {
-        if (b) {
-          System.out.print("1 ");
-        }
-        else System.out.print("0 ");
+        if (b) System.out.print("1 ");
+        else System.out.print("- ");
       }System.out.print('\n');
     }
   }
--- cw6/HGraph.java
+++ cw6/HGraph.java
@@ -4,39 +4,82 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Collections;
-import java.io.*;
 
 public class HGraph {
 
-  private boolean[][] graphTable;
-  private List<Integer> hCycle;
+  private boolean[][] graphTable; // M_ij = true -> istnieje krawędź między v_i i v_j
+  private List<Integer> hCycle; // cykl Hamiltona jako lista numerów wierzchołków
   private Random random;
-  private int verts;
+  private int verts; // liczba wierzchołków
+  private int edgeInserts; // liczba prób wstawienia dodatkowych krawędzi
 
-  public List getHcycle() { return hCycle; }
+  public List<Integer> getHCycle() { return hCycle; }
   public boolean[][] getGraphTable() { return graphTable; }
+  public int getVerts() { return verts; }
+  public int getEdgeInserts() { return edgeInserts; }
 
-  public HGraph(int verts) {
+  public void setGraphTable(boolean[][] graphTable) {
+    this.graphTable = graphTable;
+  }
+
+// konstruktor nowego grafu
+  public HGraph(int verts,int edgeInserts) {
     this.verts = verts;
+    this.edgeInserts = edgeInserts;
     graphTable = new boolean[verts][verts];
     hCycle = new ArrayList<>();
     random = new Random();
   }
 
+// konstruktor grafu izomorficznego
+// w main:
+// HGraph g = new HGraph(10,20);
+// g.generate();
+// List iso = g.isomorphism();
+// HGraph h = new HGraph(g,iso);
+  public HGraph(HGraph base, List<Integer> isomorph) {
+    this(base.getVerts(),base.getEdgeInserts());
+    // teraz trzeba przepisać graphTable uwzględniając izomorfizm. Co z cyklem???
+    boolean[][] tempGT = base.getGraphTable();
+    for (int i = 0; i < verts ; i++ ) { // for each odpada, potrzebne numery indeksów
+      for (int j = 0;j < verts ; j++ ) {
+        if (tempGT[i][j]) graphTable[isomorph.get(i)][isomorph.get(j)] = true;
+      }
+    }
+    // teraz jeszcze cykl
+    List<Integer> hc = base.getHCycle();
+    for (int i : hc) {
+      hCycle.add(isomorph.get(i));
+    }
+  }
+
   public void generate() {
-    //tworzenie losowego cyklu Hamiltona
+    // tworzenie losowej ścieżki Hamiltona
     for (int i = 0;i < verts ; i++ ) {
       hCycle.add(i);
     }
     Collections.shuffle(hCycle);
-    //zapis
-    for (int i = 0;i < hCycle.size()-1 ;) {
-      graphTable[hCycle.get(i)][hCycle.get(i++)] = true;
-      System.out.println("Inserted: "+hCycle.get(i-1)+" "+hCycle.get(i));
+    // dodanie do grafu
+    for (int i = 0;i < hCycle.size()-1 ;i++) {
+      graphTable[hCycle.get(i)][hCycle.get(i+1)] = true;
+      graphTable[hCycle.get(i+1)][hCycle.get(i)] = true;
     }
-
-    graphTable[4][6] = true;
+    // dopełnienie cyklu - krawędź między pierwszym i ostatnim wierzchołkiem
+    graphTable[hCycle.get(0)][hCycle.get(verts-1)] = true;
+    graphTable[hCycle.get(verts-1)][hCycle.get(0)] = true;
+    // dodanie dodatkowych krawędzi
+    for (int i = 0;i < edgeInserts ;i++) {
+      int r1 = random.nextInt(verts);
+      int r2 = random.nextInt(verts);
+      graphTable[r1][r2] = graphTable[r2][r1] = true;
+    }
   }
 
-
+  //stwórz izomorfizm
+  public List<Integer> isomorphism() {
+    // wierzchołek nr 'indeks' przechodzi na wierzchołek nr 'wartość'
+    List<Integer> izo = new ArrayList<>(hCycle);// Bo zawiera liczby od 0 do verts. I tak będzie permutowana.
+    Collections.shuffle(izo); // nowa permutacja numerów wierzchołków
+    return izo; // TODO dopisać konstruktor wykorzystujący to i gotowy graf.
+  }
 }
